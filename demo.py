c60c317f16da6a3f6f86c184cf2efd624a5f5b1d
zhaozhiwei <937089989@qq.com>
2021-12-21 16:33:37 +0800

    添加泳池边界过滤

--- demo.py
+++ demo.py
@@ -48,31 +48,25 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
 
 
 class TargetDetector:
-    def __init__(self, weight='./data/weights/best.pt', ret_drawbox=True):
+    def __init__(self, weight='./data/weights/best.pt', draw_label=True, mask_img=None):
         parser = argparse.ArgumentParser()
         parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-        parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
         parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
         parser.add_argument('--conf-thres', type=float, default=0.45, help='object confidence threshold')
         parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
         parser.add_argument('--hide-labels', type=bool, default=False, help='hide labels')
         parser.add_argument('--hide-conf', type=bool, default=False, help='hide conf')
         parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-        parser.add_argument('--view-img', action='store_true', help='display results')
-        parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-        parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-        parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+        parser.add_argument('--view-img', type=bool, default=False, help='display results')
         parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
         parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
         parser.add_argument('--augment', action='store_true', help='augmented inference')
-        parser.add_argument('--update', action='store_true', help='update all models')
-        parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-        parser.add_argument('--name', default='exp', help='save results to project/name')
-        parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+
         opt = parser.parse_args()
         self.opt = opt
-        self.drawbox = ret_drawbox
+        self.drawbox = draw_label
         imgsz = opt.img_size
+        self.mask_img = mask_img
 
         with torch.no_grad():
             # Initialize
@@ -83,8 +77,6 @@ class TargetDetector:
             self.model = attempt_load(weight, map_location=self.device)  # load FP32 model
             self.stride = int(self.model.stride.max())  # model stride
             self.imgsz = check_img_size(imgsz, s=self.stride)  # check img_size
-            # print(imgsz)
-            # print(stride)
             if self.half:
                 self.model.half()  # to FP16
 
@@ -121,6 +113,11 @@ class TargetDetector:
 
             ret = det.cpu().numpy()
 
+            # 去除异常的检测框
+            ret = ret[ret[:, -1] == 0]
+            if self.mask_img is not None:
+                ret = self.remove_outside_border(ret)
+
             if self.drawbox:
                 annotator = Annotator(im0, line_width=3, example=str('abc'))
 
@@ -131,39 +128,19 @@ class TargetDetector:
                     c = int(cls)  # integer class
                     label = None if opt.hide_labels else (names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
                     annotator.box_label(xyxy, label, color=colors[int(cls)])
-            # cv2.imshow('img', im0)
-            # cv2.waitKey(0)
-            return ret  # , im0
+            return ret
 
-    # 五颜六色，弃
-    def addBox(self, img, pred):
-        opt = self.opt
-        with torch.no_grad():
-            # Padded resize
-            im0 = img
-            img = letterbox(img, self.imgsz, stride=self.stride)[0]
+    def remove_outside_border(self, pred):
+        center = np.c_[(pred[:, 0] + pred[:, 2]) / 2, (pred[:, 1] + pred[:, 3]) / 2].astype(np.int32)
+        index = []
+        img = self.mask_img.copy()
+        for cc in center:
+            index.append(True) if self.mask_img[cc[1], cc[0]] != 0 else index.append(False)
+            # if self.mask_img[cc[1], cc[0]] != 0:
+            #     cv2.circle(img, (cc[0], cc[1]), radius=3, color=(255, 255, 255), thickness=4)
+            #     cv2.circle(im0, (cc[0], cc[1]), radius=3, color=(255, 255, 255), thickness=4)
 
-            # Convert
-            img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-            img = np.ascontiguousarray(img)
-
-            img = torch.from_numpy(img).to(self.device)
-            img = img.half() if self.half else img.float()  # uint8 to fp16/32
-            img /= 255.0  # 0 - 255 to 0.0 - 1.0
-            if img.ndimension() == 3:
-                img = img.unsqueeze(0)
-
-            annotator = Annotator(im0, line_width=3, example=str('abc'))
-
-            # names = ['person']
-            # colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-            # for id, ret in pred.items():
-            #     # print(type(ret))
-            #     *xyxy, conf, cls = ret
-            #     # c = int(cls)  # integer class
-            #     # label = None if opt.hide_labels else (names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
-            #     annotator.box_label(xyxy, str(id), color=colors[int(cls)])
-        return im0
+        return pred[index]
 
 
 def plot_one_box(x, img, color=None, label=None, line_thickness=3):
@@ -180,20 +157,58 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
+points = []
+
+
+def on_EVENT_BUTTONDOWN(event, x, y, flags, param):
+    global points
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points.append([x, y])
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        points.pop()
+
+
+def generate_mask(img):
+    mask = np.zeros(img.shape[:2])
+    cv2.namedWindow('img', cv2.WINDOW_GUI_NORMAL)
+    cv2.setMouseCallback("img", on_EVENT_BUTTONDOWN, True)
+    # cv2.namedWindow('mask', cv2.WINDOW_GUI_NORMAL)
+    while True:
+        img_c1 = img.copy()
+        x = np.array(points, dtype=np.int32)
+        cv2.polylines(img_c1, [x], True, (0, 0, 255), thickness=2)
+        for p in x:
+            cv2.circle(img_c1, (p[0], p[1]), 2, (0, 0, 255), thickness=3)
+        if len(x) > 2:
+            cv2.fillConvexPoly(mask, x, (255, 255, 255))
+
+        cv2.imshow("img", img_c1)
+        if cv2.getWindowProperty('img', 0) == -1 or cv2.waitKey(10) == ord(' '):  # 当窗口关闭时为-1，显示时为0
+            break
+    cv2.destroyAllWindows()
+    if len(points) == 0:
+        return None
+    else:
+        return mask
+
+
 if __name__ == '__main__':
-    TD = TargetDetector('./data/weights/best_swim_1220.pt')
-    video_path = 'rtsp://admin:123@192.168.1.51:554'
+    # video_path = 'rtsp://admin:123@192.168.1.51:554'
     video_path = './data/video/20211102195133468.avi'
     cap = cv2.VideoCapture(video_path)
     ret, frame = cap.read()
+    mask = generate_mask(frame)
+
+    TD = TargetDetector('./data/weights/best_swim_1220.pt', mask_img=mask)
+
     cv2.namedWindow('src', cv2.WINDOW_GUI_NORMAL)
     while ret:
-        # box = [[500, 500, 700, 700], [100, 100, 200, 200], [500, 400, 600, 600], [0,0,200, 700]]
         pred = TD.detect(frame)
+        x = np.array(points, dtype=np.int32)
+        cv2.polylines(frame, [x], True, (0, 0, 255), thickness=2)
         cv2.imshow("src", frame)
-
         ret, frame = cap.read()
-        if cv2.waitKey(5) == ord(' '):
+        if cv2.waitKey(1) == ord(' '):
             break
     cv2.destroyAllWindows()
 
