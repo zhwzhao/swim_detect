4f533b1a74c60fb848a34a91bc040c4af7a4a8ff
zhwzhao <937089989@qq.com>
2022-01-05 18:03:31 +0800

    添加配置文件

---
+++ config.py
@@ -0,0 +1,5 @@
+ip = '192.168.1.88'
+rtsp_ip = 'rtsp://admin:123@192.168.1.51:554'
+test_video = r'./data/video/20211221143005488.avi'
+cuiniao_video = r'./data/video/20211102195133468.avi'
+save_video = False  # 是否保存视频
